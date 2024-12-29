d841b06945c5a4a36a47fc162e3442b2f94a0be0
meetinger <meetingerdev@gmail.com>
2024-12-30 00:22:32 +0300

    dockerize

--- core/settings.py
+++ core/settings.py
@@ -6,17 +6,10 @@ from project_root import PROJECT_ROOT
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=PROJECT_ROOT / '.env', extra='ignore')
 
-    BOT_TOKEN: str
-
-    # DB_USER: str
-    # DB_PASSWORD: str
-    # DB_HOST: str
-    # DB_PORT: str
-    # DB_NAME: str
+    BOT_TOKEN: str | None = None
 
     @property
     def db_url(self):
-        # return f'postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
         return f'sqlite+aiosqlite:///{PROJECT_ROOT}/database.sqlite3'
 
 settings = Settings()
